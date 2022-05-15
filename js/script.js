851cf572dbeb0dcce0253eddfad1b85c4597a5bd
Taha S. ASAN <tahaasann@gmail.com>
2022-05-15 13:01:58 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let name = document.querySelector("#name");
+        let veri = document.querySelector("#veri");
+        let buton = document.querySelector("#ekle");
+        buton.addEventListener("click",function(){
+        
+            name.innerHTML = veri.value;
+            
+            
+})
