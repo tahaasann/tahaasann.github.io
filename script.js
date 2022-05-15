f2813d82378a25062a4234ced962815cbdd5e21e
Taha S. ASAN <tahaasann@gmail.com>
2022-05-15 13:02:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let name = document.querySelector("#name");
-        let veri = document.querySelector("#veri");
-        let buton = document.querySelector("#ekle");
-        buton.addEventListener("click",function(){
-        
-            name.innerHTML = veri.value;
-            
-            
-})
