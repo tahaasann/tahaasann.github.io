37509b2ca8a90b63386685294d169cdabf560f73
Taha S. ASAN <tahaasann@gmail.com>
2022-10-26 15:08:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,25 @@ let name = document.querySelector("#name");
             
             
 })
+
+const url = req.url;
+
+    if(url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h2>INDEX SAYFASINA HOSGELDINIZ</h2>');
+    }
+    else if(url === '/hakkimda'){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('<h2>HAKKIMDA SAYFASINA HOSGELDINIZ</h2>');
+    }
+    else if(url === '/iletisim'){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('<h2>ILETISIM SAYFASINA HOSGELDINIZ</h2>');
+    }
+    else{
+        res.writeHead(404, {'Content-Type':'text/html'});
+        res.write('<h2>404 NOT FOUND</h2>');
+    }
+    res.end();
+
+});
