ce7b55a761503dab6505ae5f4321d0fa9a0c9fdb
Taha S. ASAN <tahaasann@gmail.com>
2023-01-12 01:13:45 +0300

    Create script.js

---
+++ PWGenerator/script.js
@@ -0,0 +1,31 @@
+function generatePassword() {
+    var length = document.getElementById("length").value;
+    var useUppercase = document.getElementById("uppercase").checked;
+    var useNumbers = document.getElementById("numbers").checked;
+    var useSpecial = document.getElementById("special").checked;
+  
+    var characters = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "0123456789";
+    var special = "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
+  
+    if(useUppercase) {
+      characters += characters.toUpperCase();
+    }
+  
+    if(useNumbers) {
+      characters += numbers;
+    }
+  
+    if(useSpecial) {
+      characters += special;
+    }
+  
+    var password = "";
+    for(var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+  
+    document.getElementById("password").innerHTML = password;
+  }
+  
