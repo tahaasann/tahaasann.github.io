7f24e8043427edcd4c5498fbc70f7c01fb204fd4
tahaasann <tahaasann@gmail.com>
2024-06-19 01:27:26 +0300

    New Design For My Site

--- PWGenerator/script.js
+++ PWGenerator/script.js
@@ -1,31 +1,30 @@
 function generatePassword() {
-    var length = document.getElementById("length").value;
-    var useUppercase = document.getElementById("uppercase").checked;
-    var useNumbers = document.getElementById("numbers").checked;
-    var useSpecial = document.getElementById("special").checked;
-  
-    var characters = "abcdefghijklmnopqrstuvwxyz";
-    var numbers = "0123456789";
-    var special = "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
-  
-    if(useUppercase) {
+  var length = document.getElementById("length").value;
+  var useUppercase = document.getElementById("uppercase").checked;
+  var useNumbers = document.getElementById("numbers").checked;
+  var useSpecial = document.getElementById("special").checked;
+
+  var characters = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var special = "!@#$%^&*()_+-=[]{}|;':\"<>,.?/\\";
+
+  if(useUppercase) {
       characters += characters.toUpperCase();
-    }
-  
-    if(useNumbers) {
+  }
+
+  if(useNumbers) {
       characters += numbers;
-    }
-  
-    if(useSpecial) {
+  }
+
+  if(useSpecial) {
       characters += special;
-    }
-  
-    var password = "";
-    for(var i = 0; i < length; i++) {
+  }
+
+  var password = "";
+  for(var i = 0; i < length; i++) {
       var randomIndex = Math.floor(Math.random() * characters.length);
       password += characters[randomIndex];
-    }
-  
-    document.getElementById("password").innerHTML = password;
   }
-  
+
+  document.getElementById("password").innerHTML = password;
+}
